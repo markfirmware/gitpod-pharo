a41270f215e0975489e3a8b8eb8cc9da0c69e500
markfirmware <mark.kendrat@gmail.com>
2021-02-14 03:36:20 +0000

    pre-built vscode-pharo image

--- get-pharo.sh
+++ get-pharo.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-curl -s -o pharo-launcher.zip -L https://files.pharo.org/pharo-launcher/linux64
-unzip -q pharo-launcher.zip
-rm pharo-launcher.zip
+rm -rf pharo
+mkdir pharo
+cd pharo
+wget -q https://github.com/badetitou/Pharo-LanguageServer/releases/download/v1.0.3/Pharo64-9.0-PLS.zip
+wget -q https://files.pharo.org/vm/pharo-spur64/Linux-x86_64/PharoVM-9.0.0-f519c4c-Linux-x86_64-stockReplacement-bin.zip
+
+unzip -q Pharo64*.zip
+unzip -q PharoVM*.zip
--- get-vscode-pharo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-account=badetitou
-mkdir -p github.com/$account
-cd github.com/$account
-for repo in vscode-pharo Pharo-LanguageServer
-do
-   git clone https://github.com/$account/$repo
-done
